1c4a9676158fd0974ab54672fca13fd3af0b9e11
Cameron <cameroncronheimer@gmail.com>
2022-04-24 01:41:33 -0400

    update

--- client/src/apis/CodeFinder.js
+++ client/src/apis/CodeFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:80/api/v1/code"
+    baseURL: "http://codemates.us-east-1.elasticbeanstalk.com/api/v1/code"
 })
